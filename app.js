42ffe2ad833e339d48cfa4f7fd71823c327f943b
Vasco Matos <vascomato@gmail.com>
2020-05-06 18:17:08 +0100

    normal iterations done. Starting the Bonus

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const hbs = require('hbs');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -21,5 +22,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  /* punkAPI
+    .getBeers(beersFromApi)
+    .then(beer => {
+      const data = beer.data;
+      const beerSearch = data.Search;
+      res.render('beers', { beerSearch });
+    })
+    .catch(error => {
+      console.log('Error loading response from api');
+      console.log(error);
+    });*/
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    const randomBeer = punkAPI.getRandom();
+    randomBeer.then(beer => {
+      console.log(beer);
+      res.render('random-beers', { beer });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+
+/*
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom(beersFromApi)
+  .then(responseFromAPI => {
+    // your magic happens here
+    const beerAPI = [];
+    for (let i = 0; i<beersFromApi.length; i++){
+      beerAPI.push(punkAPI.beers(i));
+    }
+    response.render('random-beers', {
+      randomBeers: beerAPI
+    });
+  })
+  .catch(error => console.log(error));
+});*/
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
